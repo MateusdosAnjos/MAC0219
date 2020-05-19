2bc098d0d5cb4fab9d6b332bbbef2c83bb6b957d
Rodrigo Ney <rodrigoney.professional@gmail.com>
2020-05-19 02:29:30 -0300

    fix run_measurements image size increment for thread loop

--- EP1/src/run_measurements.sh
+++ EP1/src/run_measurements.sh
@@ -24,8 +24,8 @@ for NAME in ${NAMES[@]}; do
                 perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE $j >> seahorse.log 2>&1
                 perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE $j >> elephant.log 2>&1
                 perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE $j >> triple_spiral.log 2>&1
-                SIZE=$(($SIZE * 2))
             done
+            SIZE=$(($SIZE * 2))
         else
             perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
             perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
